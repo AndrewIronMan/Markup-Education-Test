b3a9e01bbb7a7ad571d102ceadc53e9483a3da67
jc_astarodubtsev <astarodubtsev@justcoded.co>
2021-07-06 23:30:57 +0300

    feat: service section without slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -178,6 +178,12 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).ready(function () {
     prevArrow: '<button id="prev" type="button" class="arrow-btn "><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
     nextArrow: '<button id="next" type="button" class="arrow-btn arrow-btn--right "><i class="fa fa-chevron-left" aria-hidden="true"></i></button>'
   });
+});
+jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).ready(function () {
+  jquery__WEBPACK_IMPORTED_MODULE_0___default()('.content-slider').slick({
+    slidesToShow: 1,
+    dots: true
+  });
 }); // });
 
 /***/ })
--- src/js/app.js
+++ src/js/app.js
@@ -17,4 +17,14 @@ $(document).ready(() => {
       '<button id="next" type="button" class="arrow-btn arrow-btn--right "><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
   });
 });
+
+
+
+$(document).ready(() => {
+  $('.content-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+  });
+})
+
 // });
